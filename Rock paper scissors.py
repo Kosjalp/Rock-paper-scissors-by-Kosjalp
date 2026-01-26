0e46d5ead126f4c6b13e31e05154bda269b83485
Kosjalp <janalpkosar@gmail.com>
2026-01-26 12:45:38 +0100

    Add more info in code

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -1,16 +1,20 @@
 #Rock paper scissors
 #By Kosjalp
+
+#Import libraries
 import time
 import random
 import getpass
 import os
 import datetime
+#Define variables
 wins = 0
 ties = 0
 losses = 0
 computer_input = ("UNDEFINED")
 choices = ["r", "p", "s"]
 rounds = 0
+#Get user data
 user = getpass.getuser()
 time_at_logon = datetime.datetime.now().replace(microsecond=0)
 #Write files
@@ -22,6 +26,7 @@ else:
     h.write("\n")
 with open("history.txt", "a") as h:
   h.write(f"User {user} logged on at date {time_at_logon}\n")
+#Loading sequence
 print("Welcome to rock paper scissors!")
 time.sleep(1)
 print("Let me set things up real quick...")
@@ -43,9 +48,11 @@ print("")
 time.sleep(2)
 for i in range(3):
   print("")
+#Looping sequence
 while True:
-  player_input = input("Rock(r) paper(p) scissors(s) quit(q) or stats(i)? ")
-  computer_input = random.choice(choices)
+  player_input = input("Rock(r) paper(p) scissors(s) quit(q) or stats(i)? ") #Get user input
+  computer_input = random.choice(choices) #Pick random computer input
+  #Check winning/tying/losing
   if player_input == "r":
     if computer_input == "s":
       print("You win!")
@@ -99,6 +106,7 @@ while True:
       print("You win!")
       wins += 1
       rounds +=1
+  #Stats section
   elif player_input == "i":
     print("Stats:")
     print("")
@@ -106,13 +114,17 @@ while True:
     print("Losses", losses)
     print("Ties", ties)
     print("")
-    score = float((wins+1)/((losses+1)*2)+(ties/2)-(losses/2))
+    score = float((wins+1)/((losses+1)*2)+(ties/2)-(losses/2)) #Calculate overall score
     print("Overall score:", score)
+  #Quitting
   elif player_input == "q":
     print("Manual quit")
     for i in range(3):
       print("")
     quit("Thanks for playing!")
+  #Error
+  #Error01: Computer gives invalid input
+  #Error02: Player gives invald input
   else:
     for i in range(3):
       print("")
@@ -120,4 +132,3 @@ while True:
     print("I don't understand... There's clearly a misunderstanding...")
     for i in range(3):
       print("")
-  
