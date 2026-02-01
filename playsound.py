246dffc0c70d02eb709192fbbd2eb943e8961620
Kosjalp <janalpkosar@gmail.com>
2026-02-01 19:48:58 +0100

    Fix windows playsound bug
    accidentally didn't commit before

--- playsound.py
+++ playsound.py
@@ -1,8 +1,13 @@
 import os
+
 def playsound(user_os, playsoundfilepath):
   if user_os == "UNIX":
     os.system(f"afplay {playsoundfilepath}")
   elif user_os == "Linux":
     os.system(f"aplay {playsoundfilepath}")
   elif user_os == "Windows":
-    os.system(f"start {playsoundfilepath}")
+    import winsound
+    try:
+      winsound.PlaySound(playsoundfilepath, winsound.SND_FILENAME)
+    except Exception as e:
+      print(f"Error playing sound: {e}")
