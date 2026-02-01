bfe2f2583131be0f0ed6de4afa19fff160b72d81
Kosjalp <janalpkosar@gmail.com>
2026-02-01 09:57:27 +0100

    Add support for Linux sound playback

--- playsound.py
+++ playsound.py
@@ -2,5 +2,7 @@ import os
 def playsound(user_os, playsoundfilepath):
   if user_os == "UNIX":
     os.system(f"afplay {playsoundfilepath}")
+  elif user_os == "Linux":
+    os.system(f"aplay {playsoundfilepath}")
   elif user_os == "Windows":
     os.system(f"start {playsoundfilepath}")
