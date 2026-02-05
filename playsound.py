2223819cb6f0f7726f5f3660f5e2e652f9028069
Kosjalp <janalpkosar@gmail.com>
2026-02-05 12:26:59 +0100

    Add wider linux compatibility

--- playsound.py
+++ playsound.py
@@ -3,6 +3,6 @@ def playsound(user_os, playsoundfilepath):
   if user_os == "Mac":
     os.system(f"afplay {playsoundfilepath}")
   elif user_os == "Linux":
-    os.system(f"aplay {playsoundfilepath}")
+    os.system(f"ffplay {playsoundfilepath}")
   elif user_os == "Windows":
     os.system(f"start {playsoundfilepath}")
