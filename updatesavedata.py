147f71e80e256916a66459a3ccb4769656c85253
Kosjalp <janalpkosar@gmail.com>
2026-02-12 13:00:38 +0100

    Add achievements to updatesavedata

--- updatesavedata.py
+++ updatesavedata.py
@@ -1,4 +1,6 @@
-def updatesavedata(wins, money, losses, ties):
+import os
+def updatesavedata(wins, money, losses, ties, achievements):
+  os.makedirs("save_data", exist_ok=True)
   with open("save_data/money", "w") as h:
     h.write(str(money))
   with open("save_data/wins", "w") as h:
@@ -7,3 +9,9 @@ def updatesavedata(wins, money, losses, ties):
     h.write(str(losses))
   with open("save_data/ties", "w") as h:
     h.write(str(ties))
+  with open("save_data/achievements", "w") as h:
+    for a in achievements:
+        if isinstance(a, list):
+            h.write(f"{a}\n")
+        else:
+            h.write(f"{a}\n")
